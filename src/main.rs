430b1cfcdee0f4dafe47a01561daa44bea952eb8
rem <doitrem@gmail.com>
2023-01-18 21:01:01 +0400

    edit guess.trim / add 1st version

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,9 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
+        //let guess: u32 = guess.trim().parse().expect("Please type a number!"); // 1st version
+
+
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
